fe78fa8963b37b8d3162e2edccdff9e69af0a970
Jayanth VD <jayanthvd25@gmail.com>
2026-07-16 12:29:31 +0530

    count down timer project is initiated

---
+++ Countdown-timer/count.js
@@ -0,0 +1,8 @@
+let hours = document.querySelector("#hours");
+let minutes = document.querySelector("#minutes");
+let seconds = document.querySelector("#seconds");
+
+let startButton = document.querySelector("start");
+let pauseButton = document.querySelector("pause");
+let resetButton = document.querySelector("reset");
+
